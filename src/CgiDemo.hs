834f3a43d49f27f579679e0b42930b510b7c3371
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-03-21 10:40:45 +0100

    Removed all the demo stuff from salvia-extras, this will now be in salvia-demo.

--- src/CgiDemo.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Network.Salvia.Impl.Cgi
-import Network.Salvia.Handler.ExtendedFileSystem
-
-main :: IO ()
-main = start "/code/salvia-extras" (hCgiEnv (hExtendedFileSystem ".")) ()
-
